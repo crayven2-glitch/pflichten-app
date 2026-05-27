0a522933cd9e361d9d6690262d0079121e23a588
crayven2-glitch <crayven2@gmail.com>
2026-05-27 22:02:54 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -36,7 +36,7 @@ self.addEventListener('notificationclick', e => {
 });
 
 // ── Cache (PWA offline) ──
-const CACHE_NAME = 'pflichten-v21';
+const CACHE_NAME = 'pflichten-v22';
 const ASSETS = [
   './',
   './index.html',
